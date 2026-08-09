81bdf1b78eb302141508ac473d7871930a9ae5dc
medaziz701 <zz.chaabani@gmail.com>
2026-08-09 10:59:29 +0200

    Clean migrations and configure PostgreSQL with SQLite fallback

--- tunisie_telecom/tunisie_telecom/settings.py
+++ tunisie_telecom/tunisie_telecom/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'tunisie_telecom.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DB_HOST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
